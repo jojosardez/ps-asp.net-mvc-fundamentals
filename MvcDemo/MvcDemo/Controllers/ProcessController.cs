f8207f0d2dd223848393250590596f8a17cb1588
Angelito Sardez, Jr <jojosardez@gmail.com>
2017-09-30 18:29:43 +0800

    Contains view and controller for Process.

---
+++ MvcDemo/MvcDemo/Controllers/ProcessController.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace MvcDemo.Controllers
+{
+    public class ProcessController : Controller
+    {
+        // GET: Process/List
+        public ViewResult List()
+        {
+            var processList = from p in Process.GetProcesses()
+                select p;
+            
+            return View(processList.ToList());
+        }
+
+        // GET: Process/Details/5
+        public ActionResult Details(int id)
+        {
+            var process = (from p in Process.GetProcesses()
+                where p.Id == id
+                select p).Single();
+
+            return View(process);
+        }
+    }
+}
