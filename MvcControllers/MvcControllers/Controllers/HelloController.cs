3af51887ada56677b978aedd49f6caeaefdfc5e5
Angelito Sardez, Jr <jojosardez@gmail.com>
2017-10-05 23:33:24 +0800

    Routing to controller and IController implementation.

---
+++ MvcControllers/MvcControllers/Controllers/HelloController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MvcControllers.Controllers
+{
+    public class HelloController : IController
+    {
+        public void Execute(RequestContext requestContext)
+        {
+            requestContext.HttpContext.Response.Write("Hello, World");
+        }
+    }
+}
