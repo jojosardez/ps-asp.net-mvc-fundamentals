99120199be8090ed72bb81f59add191191add608
Angelito Sardez, Jr <jojosardez@gmail.com>
2017-10-06 01:51:52 +0800

    Custom controller action filter.

--- MvcControllers/MvcControllers/Controllers/Hello2Controller.cs
+++ MvcControllers/MvcControllers/Controllers/Hello2Controller.cs
@@ -1,9 +1,11 @@
-﻿using System.Web.Mvc;
+﻿using MvcControllers.Infrastructure;
+using System.Web.Mvc;
 
 namespace MvcControllers.Controllers
 {
     public class Hello2Controller : Controller
     {
+        [Log]
         public string SayHello(int id)
         {
             return "Hello, World! v2.0 " + id.ToString();
---
+++ MvcControllers/MvcControllers/Infrastructure/LogAttribute.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MvcControllers.Infrastructure
+{
+    public class LogAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            Log("Action Executing", filterContext.RouteData);
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            Log("Action Executed", filterContext.RouteData);
+        }
+
+        public override void OnResultExecuting(ResultExecutingContext filterContext)
+        {
+            Log("Result Executing", filterContext.RouteData);
+        }
+
+        public override void OnResultExecuted(ResultExecutedContext filterContext)
+        {
+            Log("Result Executed", filterContext.RouteData);
+        }
+
+        private void Log(string stageName, RouteData routeData)
+        {
+            Debug.WriteLine($"{routeData.Values["controller"]}::{routeData.Values["action"]} - {stageName}");
+        }
+    }
+}
