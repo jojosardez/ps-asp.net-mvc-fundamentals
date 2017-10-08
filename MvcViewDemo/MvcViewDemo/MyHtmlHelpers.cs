770eb8fd729c0bd2dda2135e40c364f331f8a962
Angelito Sardez, Jr <jojosardez@gmail.com>
2017-10-08 13:00:07 +0800

    Custom \ extension helper implementation.

---
+++ MvcViewDemo/MvcViewDemo/MyHtmlHelpers.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+
+namespace MvcViewDemo
+{
+    public static class MyHtmlHelpers
+    {
+        public static TagBuilder Image(this HtmlHelper helper, string imageUrl, string alt)
+        {
+            if (!string.IsNullOrEmpty(imageUrl))
+            {
+                TagBuilder imageTag = new TagBuilder("img");
+                imageTag.MergeAttribute("src", imageUrl);
+                imageTag.MergeAttribute("alt", alt);
+                return imageTag;
+            }
+            return null;
+        }
+    }
+}
