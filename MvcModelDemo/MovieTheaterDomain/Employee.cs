2cf99ec327c762162c3e09e46c860a2fc1e48a01
Angelito Sardez, Jr <jojosardez@gmail.com>
2017-10-08 16:59:16 +0800

    Added employee model and its corresponding controller and view.

---
+++ MvcModelDemo/MovieTheaterDomain/Employee.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MovieTheaterDomain
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime HireDate { get; set; }
+    }
+}
---
+++ MvcModelDemo/MovieTheaterDomain/EmployeeRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MovieTheaterDomain
+{
+    public class EmployeeRepository
+    {
+        public IQueryable<Employee> FindAll()
+        {
+            return _employees.AsQueryable();
+        }
+
+        public Employee FindByID(int id)
+        {
+            return _employees.Where(e => e.ID == id).FirstOrDefault();
+        }
+
+        public void Save(Employee employee)
+        {
+            var existingEmployee = _employees.Where(e => e.ID == employee.ID).FirstOrDefault();
+            if (existingEmployee == null)
+            {
+                _employees.Add(employee);
+            }
+        }
+
+        #region Source
+        // for demo purposes only
+        static List<Employee> _employees = new List<Employee>
+        {
+            new Employee() { ID=1, Name="Scott", HireDate=new DateTime(2001,10,15) },
+            new Employee() { ID=2, Name="Poonam", HireDate=new DateTime(2001, 3, 10) }
+        };
+        #endregion
+    }
+}
