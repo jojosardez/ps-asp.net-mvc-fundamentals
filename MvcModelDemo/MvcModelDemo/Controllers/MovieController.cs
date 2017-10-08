bef06deacecbbda2e18c682eb8ef6ee4275560b6
Angelito Sardez, Jr <jojosardez@gmail.com>
2017-10-08 18:52:18 +0800

    Added search model (and controller) functionality.

--- MvcModelDemo/MvcModelDemo/Controllers/MovieController.cs
+++ MvcModelDemo/MvcModelDemo/Controllers/MovieController.cs
@@ -7,11 +7,12 @@ namespace MvcModelDemo.Controllers
     public class MovieController : Controller
     {
         // GET: Movie
-        public ActionResult Index()
+        public ActionResult Index(string q)
         {
             using (var ctx = new MoviesContext())
             {
                 var movies = ctx.MovieSet
+                    .Where(m => m.Title.StartsWith(q) || q == null)
                     .OrderByDescending(m => m.Reviews.Average(r => r.Rating))
                     .Take(10)
                     .Select(m => new MovieSummary
